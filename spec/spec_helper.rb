c7571c19f7370c801d9d39385b9e2a1be2a98836
Ikuyadeu <ikuyadeu@yahoo.co.jp>
2015-05-04 17:11:19 +0900

    Include Bootstrap

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -50,6 +50,17 @@ Spork.prefork do
     #     --seed 1234
     config.order = "random"
     config.include Capybara::DSL
+
+    # rspec-rails 3 will no longer automatically infer an example group's spec type
+    # from the file location. You can explicitly opt-in to the feature using this
+    # config option.
+    # To explicitly tag specs without using automatic inference, set the `:type`
+    # metadata manually:
+    #
+    #     describe ThingsController, :type => :controller do
+    #       # Equivalent to being in spec/controllers
+    #     end
+    config.infer_spec_type_from_file_location!
   end
 end
 
