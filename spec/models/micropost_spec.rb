2fa207678a27999705325c53c3b80c7e22e68e74
Ikuyadeu <ikuyadeu@yahoo.co.jp>
2015-05-20 13:38:45 +0900

    show micropost

---
+++ spec/models/micropost_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe Micropost, type: :model do
+ let(:user) { FactoryGirl.create(:user) }
+ before do
+  @micropost = user.microposts.build(content: "Lorem ipsum")
+ end
+
+ subject { @micropost }
+ 
+ it { is_expected.to respond_to(:content) }
+ it { is_expected.to respond_to(:user_id) }
+ it { is_expected.to respond_to(:user) }
+
+ describe '#user' do
+   subject { super().user }
+   it { is_expected.to eq user}
+ end
+
+ it {is_expected.to be_valid}
+
+ describe "when user_id is not present" do
+  before {@micropost.user_id = nil}
+  it {is_expected.not_to be_valid}
+ end
+
+  describe "when user_id is not present" do
+    before {@micropost.user_id = nil }
+    it { is_expected.not_to be_valid }
+  end
+
+  describe "with blank content" do
+    before {@micropost.content = " "}
+    it { is_expected.not_to be_valid }
+  end
+
+  describe "with blank content" do
+    before {@micropost.content = "a" * 141 }
+    it { is_expected.not_to be_valid }
+  end     
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -32,7 +32,7 @@ describe "User pages" do
 
     describe "delete links" do
 
-      it { should_not have_link('delete') }
+      it { is_expected.not_to have_link('delete') }
 
       describe "as an admin user" do
         let(:admin) { FactoryGirl.create(:admin) }
@@ -41,13 +41,13 @@ describe "User pages" do
           visit users_path
         end
 
-        it { should have_link('delete', href: user_path(User.first)) }
+        it { is_expected.to have_link('delete', href: user_path(User.first)) }
         it "should be able to delete another user" do
           expect do
             click_link('delete', match: :first)
           end.to change(User, :count).by(-1)
         end
-        it { should_not have_link('delete', href: user_path(admin)) }
+        it { is_expected.not_to have_link('delete', href: user_path(admin)) }
       end
     end
   end 
@@ -62,11 +62,20 @@ describe "User pages" do
   end
 
   describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
+    let(:user) {FactoryGirl.create(:user) }
+    let(:m1) {FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let(:m2) {FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
+    before { visit user_path(user)}
 
     it { is_expected.to have_content(user.name) }
     it { is_expected.to have_title(user.name) }
+
+    describe "microposts" do
+      it { is_expected.to have_content(m1.content) }
+      it { is_expected.to have_content(m2.content) }
+      it { is_expected.to have_content(user.microposts.count) }
+    end
   end
 
   describe "signup" do
